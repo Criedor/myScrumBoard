90980ee8839039d6d29aaa501f0085904cd8e0c5
Christoph Steiner <gawen@t-online.de>
2020-04-13 12:38:06 +0200

    all lot of functions

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var divCounter = 0;
 var selectedID = 0;
-var show = document.getElementById("showNew")
+var show = document.getElementById("showNew");
+var selectedColumn = document.getElementsByTagName("option")[0].getAttribute("value");
+const backlogGrid  = document.getElementsByClassName("backlog_grid")[0]
+const toDoGrid = document.getElementsByClassName("toDo_grid")[0]
+const inProgressGrid = document.getElementsByClassName("inProgress_grid")[0]
+const doneGrid = document.getElementsByClassName("done_grid")[0]
+
+var item = 0
+
+backlogGrid.addEventListener("click", getId)
+doneGrid.addEventListener("click", getId)
+inProgressGrid.addEventListener("click", getId)
+toDoGrid.addEventListener("click", getId)
 
 function makeDiv () {
-    const backlogGrid  = document.getElementsByClassName("backlog_grid")[0]
+
+    if (show.classList.contains("display")){
+        showNew();
     var div = document.createElement("div");
     var p = document.createElement("p");
     var t = document.createTextNode("")
@@ -14,6 +28,7 @@ function makeDiv () {
     div.id = divCounter;
     selectedID = divCounter
     divCounter = divCounter+1;
+    }
 }
 
 function showNew () {
@@ -37,5 +52,52 @@ function save () {
     show.classList.toggle("display");
 
     }
+}
+
+
+function moveTo() {
+    var selectedDiv = document.getElementById("selectedID")
+    var moveToColumn = document.getElementById("moveTo").selectedIndex;
+
+    switch (moveToColumn) {
+        case 0: backlogGrid.appendChild(selectedDiv);
+        break;
+        case 1: toDoGrid.appendChild(selectedDiv);
+        break;
+        case 2: inProgressGrid.appendChild(selectedDiv);
+        break;
+        case 3: doneGrid.appendChild(selectedDiv);
+        break;
+    }
+
+}
+
+
+
+function getId(i) {
+        item = i.target; 
+ 
+        switch (true) {
+        case i.target.parentElement.classList.contains("backlog_grid"): selectedColumn = 0;
+        break;
+
+        case i.target.parentElement.classList.contains("toDo_grid"): selectedColumn = 1;
+        break;
+
+        case i.target.parentElement.classList.contains("inProgress_grid"): selectedColumn = 2;
+        break;
+        
+        case i.target.parentElement.classList.contains("done_grid"): selectedColumn = 3;
+        break;
+        }
+        edit();
+}
+
+
+
+function edit() {
+        showNew ()
+        document.getElementsByClassName("content")[1].value = document.getElementById(item.id).firstChild.innerHTML;
+        document.getElementById("moveTo").selectedIndex = selectedColumn
 
 }
